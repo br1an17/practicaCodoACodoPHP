36c7c107925e17a454ba46db00eb9741b92bcc7e
br1an17 <106201381+br1an17@users.noreply.github.com>
2024-06-08 21:31:46 -0300

    empezamos con el formulario de la clase 6

--- php/clase2php/datos.php
+++ php/clase2php/datos.php
@@ -7,14 +7,14 @@ var_dump($_POST);
 
 echo "<hr>";
 
-echo "El usuario es: ".  $_POST['nombre'];
+echo "El usuario es: " .  $_POST['nombre'];
 echo "el apellido es: " . $_POST['apellido'];
 echo "el password es: " . $_POST['password'];
 echo "cantidad Cards: " . $_POST['cantidadCards'];
 
 $nombre = $_POST['nombre'];
 $edad = $_POST['edad'];
-$apellido = $_POST['apellido']; 
+$apellido = $_POST['apellido'];
 $tipo_usser = $_POST['tiposuario'];
 $descripcion = $_POST['descripcion'];
 $cantidadCards = $_POST['cantidadCards'];
@@ -32,78 +32,81 @@ echo "<br>";
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <div class="container">
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <h1>Hello, world!</h1>
+
+  <div class="container">
 
     <div class="card" style="width: 18rem;">
-  <img src="https://yt3.googleusercontent.com/vRF8BHREiJ3Y16AbMxEi_oEuoQlnNNqGpgULuZ6zrWSAi24HcxX3Vko42RN8ToctH-G0qlWd=s900-c-k-c0x00ffffff-no-rj" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title"> <?php echo $nombre ?></h5>
-    <p class="card-text"> <?php echo $descripcion ?></p>
-    <a href="index.php" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
+      <img src="https://yt3.googleusercontent.com/vRF8BHREiJ3Y16AbMxEi_oEuoQlnNNqGpgULuZ6zrWSAi24HcxX3Vko42RN8ToctH-G0qlWd=s900-c-k-c0x00ffffff-no-rj" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"> <?php echo $nombre ?></h5>
+        <p class="card-text"> <?php echo $descripcion ?></p>
+        <a href="index.php" class="btn btn-primary">Go somewhere</a>
+      </div>
     </div>
 
+  </div>
 
-<?php
 
-    if($nombre=="alejandro" && $edad>=18 && $tipo_usser=="premium"){
+  <?php
+
+  if ($nombre == "alejandro" && $edad >= 18 && $tipo_usser == "premium") {
 
     echo "Soy el admin"; ?>
 
 
-    
-     <div class="card" style="width: 18rem;">
+
+    <div class="card" style="width: 18rem;">
       <img src="https://yt3.googleusercontent.com/  vRF8BHREiJ3Y16AbMxEi_oEuoQlnNNqGpgULuZ6zrWSAi24HcxX3Vko42RN8ToctH-G0qlWd=s900-c-k-c0x0  0ffffff-no-rj" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title"> <?php echo $nombre ?></h5>
         <p class="card-text"> <?php echo $descripcion ?></p>
         <a href="index.php" class="btn btn-primary">Go somewhere</a>
-        </div>
       </div>
-   
+    </div>
 
-<?php
 
-    }else{
-      
-      echo "no sos usuario administrador";
-      ?>;
+    <?php
 
-      <div class="alert alert-primary" role="alert">
-        A simple primary alert—check it out!
-      </div>
-      <?php
+  } else {
 
-    }
+    echo "no sos usuario administrador";
+    ?>;
 
-?>
+    <div class="alert alert-primary" role="alert">
+      A simple primary alert—check it out!
+    </div>
+  <?php
 
-    <!-- Optional JavaScript; choose one of the two! -->
+  }
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  ?>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
 
 
@@ -161,27 +164,27 @@ echo "<br>";
 
 
 //........0 <  4.............
-for($i=0;$i<$cantidadCards;$i++){
+for ($i = 0; $i < $cantidadCards; $i++) {
 
   echo "probando ciclo for";
 ?>
 
-     <div class="card" style="width: 18rem;">
-      <img src="https://img.freepik.com/foto-gratis/mano-usando-computadora-portatil-pantalla-virtual-documento-aprobar-linea-concepto-gestion-erp-garantia-calidad-papel_616485-61.jpg?t=st=1717685991~exp=1717689591~hmac=cc8ecd5a808621cf27cdf2064f72334d9d9e708c5d5d9a5d9f8dba0a5bb03c8d&w=1380" class="card-img-top" alt="...">
-      <div class="card-body">
-      <span class="badge rounded-pill bg-info text-dark"> <?php echo $i +1 ?></span>
-        <h5 class="card-title"> <?php echo $nombre ?></h5>
-        <p class="card-text"> <?php echo $descripcion ?></p>
-        <a href="index.php" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
+  <div class="card" style="width: 18rem;">
+    <img src="https://img.freepik.com/foto-gratis/mano-usando-computadora-portatil-pantalla-virtual-documento-aprobar-linea-concepto-gestion-erp-garantia-calidad-papel_616485-61.jpg?t=st=1717685991~exp=1717689591~hmac=cc8ecd5a808621cf27cdf2064f72334d9d9e708c5d5d9a5d9f8dba0a5bb03c8d&w=1380" class="card-img-top" alt="...">
+    <div class="card-body">
+      <span class="badge rounded-pill bg-info text-dark"> <?php echo $i + 1 ?></span>
+      <h5 class="card-title"> <?php echo $nombre ?></h5>
+      <p class="card-text"> <?php echo $descripcion ?></p>
+      <a href="index.php" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
 
-      <div class="alert alert-primary" role="alert">
-        A simple primary alert—check it out!
-       </div>
+  <div class="alert alert-primary" role="alert">
+    A simple primary alert—check it out!
+  </div>
 
-      
-      <?php
+
+<?php
 }
 ?>
 
@@ -191,22 +194,22 @@ for($i=0;$i<$cantidadCards;$i++){
 
 echo "<hr>";
 
-for($i=0;$i<$cantidadCards;$i++){
+for ($i = 0; $i < $cantidadCards; $i++) {
 
   echo $i;
 
 ?>
 
 
-      <div class="alert alert-primary" role="alert">
-        Numero de Card:  <?php echo $i +1 ?>
+  <div class="alert alert-primary" role="alert">
+    Numero de Card: <?php echo $i + 1 ?>
 
-        <span class="badge rounded-pill bg-info text-dark"> <?php echo $i +1 ?></span>
+    <span class="badge rounded-pill bg-info text-dark"> <?php echo $i + 1 ?></span>
 
-       </div>
+  </div>
 
-      
-      <?php
+
+<?php
 }
 ?>
 
@@ -225,59 +228,50 @@ $nombres = "alejandro";
 echo "<hr>";
 echo "el tipo de dato ingresado es: ", gettype($numero);
 echo "<hr>";
-if(is_int($numero) && $numero >= 0){
+if (is_int($numero) && $numero >= 0) {
 
   echo "Es un numero!!!";
-
-}else {
+} else {
   echo "No es un numero";
 }
 
-if(is_float($numero)){
-
+if (is_float($numero)) {
 }
 
-if(is_string($numero)){
-  
+if (is_string($numero)) {
 }
 
-if(is_numeric($numero)){
-  
+if (is_numeric($numero)) {
 }
 
-if(is_bool($numero)){
-  
+if (is_bool($numero)) {
 }
 
-if(is_array($numero)){
-  
+if (is_array($numero)) {
 }
 
-if(is_object($numero)){
-  
+if (is_object($numero)) {
 }
 
- $nombres = ["juan","marcos","martin","pedro","felipe","brian","pablo"];
+$nombres = ["juan", "marcos", "martin", "pedro", "felipe", "brian", "pablo"];
 
 
- $apellido = ["juan","marcos","martin","pedro","felipe","brian","pablo"];
+$apellido = ["juan", "marcos", "martin", "pedro", "felipe", "brian", "pablo"];
 
- echo $nombres[0];
+echo $nombres[0];
 
- echo count($nombres);
+echo count($nombres);
 
- for($i=0;$i<count($nombres);$i++){
+for ($i = 0; $i < count($nombres); $i++) {
 
   echo $nombres[$i];
+}
 
- }
-
- foreach($nombres as $indice){
+foreach ($nombres as $indice) {
 
   echo "<br>";
   echo $indice;
-
- }
+}
 
 //  //scope..
 // echo $indice;
@@ -290,10 +284,10 @@ echo "<hr>";
 
 $datos = [
 
-  "nombre"=>"leandro",
-  "apellido"=>"wagner",
-  "edad"=>38,
-  "aprobado"=>true
+  "nombre" => "nonono",
+  "apellido" => "mo",
+  "edad" => 38,
+  "aprobado" => true
 
 ];
 
@@ -308,16 +302,16 @@ echo $datos["apellido"];
 
 $peliculas = [
 
-  "titulo"=>"matrix",
-  "descripcion"=>"pelicula basada en ..",
-  "genero"=>"accion",
-  "cantidadEstrellas"=>5,
-  "director"=>true
+  "titulo" => "matrix",
+  "descripcion" => "pelicula basada en ..",
+  "genero" => "accion",
+  "cantidadEstrellas" => 5,
+  "director" => true
 ];
 
 
 //otra forma de crear array por indice
-$apellidos = array(1,2,3);
+$apellidos = array(1, 2, 3);
 
 $apellido[0];
 //fin ejemplo
@@ -349,9 +343,4 @@ a) cuando presione enviar.. deberia mostrarse la informacion en un archivo del t
 */
 
 ?>
-
-
-
-
-
 
--- php/clase2php/index.php
+++ php/clase2php/index.php
@@ -39,8 +39,8 @@ $suma = 90;
 
 // // + - * / %..
 
-$resto1 = 25%5;
-$resto2 = 10%3;
+$resto1 = 25 % 5;
+$resto2 = 10 % 3;
 
 
 echo "<br>";
@@ -51,62 +51,113 @@ echo "<h1>Hola soy un h1 </h1>";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
 
-<h1>Hola esto es hmtl + php 🌷</h1>
+    <h1>Hola esto es hmtl + php 🌷</h1>
 
-<h2>bievenido <?php echo $nombre ?></h2>
+    <h2>bievenido <?php echo $nombre ?></h2>
 
-<h3>Hola formulario</h3>
+    <h3>Hola formulario</h3>
 
-<form action="datos.php" method="post">
+    <form action="datos.php" method="post">
 
-    <label for="">Nombre</label>
+        <label for="">Nombre</label>
 
-    <input type="text" name="nombre">
+        <input type="text" name="nombre">
 
-    <label for="">edad</label>
+        <label for="">edad</label>
 
-    <input type="number" name="edad">
+        <input type="number" name="edad">
 
-    <label for="">Tipo Usuario</label>
+        <label for="">Tipo Usuario</label>
 
-    <input type="text" name="tiposuario">
-
-
-    <select name="usuario_select" id="">
-
-        <option value="premium">comun</option>
-        <option value="premium">premium</option>
-        <option value="premium">platino</option>
-
-    </select>
+        <input type="text" name="tiposuario">
 
 
+        <select name="usuario_select" id="">
 
-    <label for="">Apellido</label>
+            <option value="premium">comun</option>
+            <option value="premium">premium</option>
+            <option value="premium">platino</option>
 
-    <input type="text" name="apellido">
+        </select>
 
-    <label for="">Password</label>
 
-    <input type="password" name="password">
 
-    <textarea name="descripcion" id=""></textarea>
+        <label for="">Apellido</label>
 
-    <input type="submit" value = "enviar">
+        <input type="text" name="apellido">
 
-    <label for="">Cantidad Cards</label>
+        <label for="">Password</label>
 
-    <input type="number" name="cantidadCards">
+        <input type="password" name="password">
 
-</form>
+        <textarea name="descripcion" id=""></textarea>
 
-    
+        <input type="submit" value="enviar">
+
+        <label for="">Cantidad Cards</label>
+
+        <input type="number" name="cantidadCards">
+
+    </form>
+
+    <form action="datos.php" method="post">
+        <label for="">
+            Genero
+
+            <input type="text">
+        </label>
+        <label for="">
+            Calificacion
+            <input type="number">
+
+        </label>
+        <label for="">
+            Año
+
+            <input type="number">
+        </label>
+        <label for="">
+            Director
+            <input type="text">
+
+        </label>
+
+        <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Nombre</label>
+        <input class="form-control" type="text" placeholder="ingrese el nombre de la pelicula" aria-label="default input example">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="ingrese la descripcion de la pelicula"></textarea>
+</div>
+        <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Genero</label>
+        <input class="form-control" type="text" placeholder="Default input" aria-label="default input example">
+</div>
+        <div class="mb-3">
+        <input class="form-control" type="text" placeholder="Default input" aria-label="default input example">
+</div>
+        <div class="mb-3">
+        <input class="form-control" type="text" placeholder="Default input" aria-label="default input example">
+</div>
+<div class="mb-3">
+  <label for="formFile" class="form-label">Default file input example</label>
+  <input class="form-control" type="file" id="formFile">
+</div>
+<div class="col-auto">
+    <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
+  </div>
+    </form>
 </body>
+
 </html>
